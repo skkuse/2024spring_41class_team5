25e86a496e4e723e853185b6753028cb85fbae28
fine-pine <lsm3645@g.skku.edu>
2024-05-19 21:15:47 +0900

    feat: add pages routing

--- FE/src/app/_components/Header.tsx
+++ FE/src/app/_components/Header.tsx
@@ -1,25 +1,45 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <header className="w-full flex items-center justify-between">
-      <h1>My App</h1>
+    <header className="w-full max-w-12c h-16 flex items-center justify-between">
+      <h1 className="font-bold text-green-800">Eco Merge</h1>
       <nav>
         <ul className="flex gap-8">
-          <li>
+          <li className="flex">
             <Link href="/">Home</Link>
+            {pathname.split("/")[1] === "" && (
+              <div className="absolute translate-x-1 -translate-y-0.5 rounded w-1 h-1 bg-green-800" />
+            )}
           </li>
-          <li>
+          <li className="flex">
             <Link href="/dashboard">Dashboard</Link>
+            {pathname.split("/")[1] === "dashboard" && (
+              <div className="absolute translate-x-1 -translate-y-0.5 rounded w-1 h-1 bg-green-800" />
+            )}
           </li>
-          <li>
+          <li className="flex">
             <Link href="/login">Login</Link>
+            {pathname.split("/")[1] === "login" && (
+              <div className="absolute translate-x-1 -translate-y-0.5 rounded w-1 h-1 bg-green-800" />
+            )}
           </li>
-          <li>
+          <li className="flex">
             <Link href="/signup">Sign Up</Link>
+            {pathname.split("/")[1] === "signup" && (
+              <div className="absolute translate-x-1 -translate-y-0.5 rounded w-1 h-1 bg-green-800" />
+            )}
           </li>
-          <li>
+          <li className="flex">
             <Link href="/profile">Profile</Link>
+            {pathname.split("/")[1] === "profile" && (
+              <div className="absolute translate-x-1 -translate-y-0.5 rounded w-1 h-1 bg-green-800" />
+            )}
           </li>
         </ul>
       </nav>
--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-[100dvh] w-full max-w-12c m-auto flex-col gap-12">
-      <h1>Eco Merge</h1>
+    <main>
+      <section>home</section>
     </main>
   );
 }
