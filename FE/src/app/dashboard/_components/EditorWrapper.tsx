dba7294eb74b5dc11c24cf16e531e0ff4eb2cff0
fine-pine <lsm3645@g.skku.edu>
2024-05-20 04:49:46 +0900

    feat: add editor

---
+++ FE/src/app/dashboard/_components/EditorWrapper.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Editor } from "@monaco-editor/react";
+import { Dispatch, FormEvent, SetStateAction } from "react";
+
+interface Props {
+  setCode: Dispatch<SetStateAction<string>>;
+}
+
+export default function EditorWrapper({ setCode }: Props) {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const lines = event.currentTarget.querySelector(".view-lines");
+    const code = Array.from(lines!.children)
+      .map((line: any) => line.textContent)
+      .join("\n");
+
+    setCode(code);
+  };
+
+  return (
+    <form
+      action="#"
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4 items-end flex-1"
+    >
+      <label htmlFor="comment" className="sr-only">
+        Add your code
+      </label>
+      <Editor height="calc(100dvh - 246px)" defaultLanguage="java" value="" />
+      <button
+        type="submit"
+        className="inline-flex items-center rounded-md px-4 py-2 text-sm bg-green-900 text-white hover:bg-green-800"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
