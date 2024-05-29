057129d345a103a8a0186534fcf71ac44943597e
fine-pine <lsm3645@g.skku.edu>
2024-05-30 00:39:09 +0900

    refactor: remove unused code

--- FE/src/app/dashboard/page.tsx
+++ FE/src/app/dashboard/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { useState } from "react";
-import EditorModal from "./_components/EditorModal";
-
 export default function Page() {
-  const [isView, setIsView] = useState(true);
-
   return (
-    <main className="flex-1 flex flex-col gap-4">
-      {isView && <EditorModal setIsView={setIsView} />}
+    <main>
+      <section>dashboard</section>
     </main>
   );
 }
