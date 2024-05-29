941e20c8cee4b3b4423e07a88ec2cea069e3fc7a
fine-pine <lsm3645@g.skku.edu>
2024-05-30 00:38:30 +0900

    style: update editor button style

--- FE/src/app/editor/_components/DiffEditorWrapper.tsx
+++ FE/src/app/editor/_components/DiffEditorWrapper.tsx
@@ -43,7 +43,7 @@ export default function DiffEditorWrapper({
       />
       <button
         type="submit"
-        className="inline-flex items-center rounded-md px-4 py-2 text-sm bg-green-900 text-white hover:bg-green-800"
+        className="rounded px-4 py-2 bg-green-900 text-white hover:bg-green-800"
       >
         Submit
       </button>
