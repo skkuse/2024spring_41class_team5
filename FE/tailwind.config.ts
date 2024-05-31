a65357f330da84755f497722307b0be5b3bd6ad5
fine-pine <lsm3645@g.skku.edu>
2024-05-31 22:59:24 +0900

    merge: review and adjust codes

---
+++ FE/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        /* Add new colors */
+      },
+      fontFamily: {
+        pretendard: ['pretendard', 'gothic'],
+      },
+      spacing: {
+        '1c': '4.875rem',
+        '2c': '11.25rem',
+        '3c': '17.625rem',
+        '4c': '24rem',
+        '6c': '36.75rem',
+        '8c': '49.5rem',
+        '10c': '62.25rem',
+        '12c': '75rem',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
