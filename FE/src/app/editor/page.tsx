cab2a8893d912d72df14f8ddd34953447206e240
fine-pine <lsm3645@g.skku.edu>
2024-05-31 14:09:14 +0900

    feat: add editor apis

--- FE/src/app/editor/page.tsx
+++ FE/src/app/editor/page.tsx
@@ -5,10 +5,12 @@ import EditorModal from "./_components/EditorModal";
 
 export default function Page() {
   const [isView, setIsView] = useState(true);
+  const [runtime, setRuntime] = useState(0);
 
   return (
     <main className="flex-1 flex flex-col gap-4">
-      {isView && <EditorModal setIsView={setIsView} />}
+      <h1>Dashboard</h1>
+      {isView && <EditorModal setIsView={setIsView} setRuntime={setRuntime} />}
     </main>
   );
 }
