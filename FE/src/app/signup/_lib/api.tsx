ee0c8914591cd199b401e63cb28bf0304fd17dd2
fine-pine <lsm3645@g.skku.edu>
2024-05-31 15:12:06 +0900

    feat: add signup apis

---
+++ FE/src/app/signup/_lib/api.tsx
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param email skku email
+ * @param studentId skku student id
+ * @param password
+ * @param passwordConfirm
+ * @returns user info
+ */
+export const signup = (
+  email: string,
+  studentId: number,
+  password: string,
+  passwordConfirm: string
+) => {
+  return "signup";
+};
--- FE/src/app/signup/page.tsx
+++ FE/src/app/signup/page.tsx
@@ -1,22 +1,45 @@
+"use client";
+
+import { FormEvent } from "react";
+import { signup } from "./_lib/api";
+
 export default function Page() {
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const username = e.currentTarget.username.value;
+    const studentId = Number(e.currentTarget.studentId.value);
+    const password = e.currentTarget.password.value;
+    const passwordConfirm = e.currentTarget.passwordConfirm.value;
+
+    if (password !== passwordConfirm) {
+      alert("Password and Password Confirm are not same");
+      return;
+    }
+
+    const userInfo = signup(username, studentId, password, passwordConfirm);
+    alert(userInfo);
+  };
+
   return (
     <main className="flex flex-col gap-8 text-center w-6c mx-auto">
       <h1 className="font-bold">Welcome to Eco Merge!</h1>
-      <form action="#" className="flex flex-col gap-4">
+      <form action="#" className="flex flex-col gap-4" onSubmit={onSubmit}>
         <div className="flex items-end gap-4">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="username">User Name</label>
           <input
             className="flex-1 border-solid border-b-2 border-black"
             type="text"
-            id="name"
+            id="username"
+            required
           />
         </div>
         <div className="flex items-end gap-4">
           <label htmlFor="studentId">Student ID</label>
           <input
-            className="flex-1 border-solid border-b-2 border-black"
+            className="flex-1 border-solid border-b-2 border-black remove-arrow"
             type="number"
             id="studentId"
+            required
           />
         </div>
         <div className="flex items-end gap-4">
@@ -25,14 +48,16 @@ export default function Page() {
             className="flex-1 border-solid border-b-2 border-black"
             type="password"
             id="password"
+            required
           />
         </div>
         <div className="flex items-end gap-4">
-          <label htmlFor="password-confirm">Password Confirm</label>
+          <label htmlFor="passwordConfirm">Password Confirm</label>
           <input
             className="flex-1 border-solid border-b-2 border-black"
             type="password"
-            id="password-confirm"
+            id="passwordConfirm "
+            required
           />
         </div>
         <button
