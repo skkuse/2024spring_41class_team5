93c7903cd8fef566df3a3f11a255d2a9e06aac4c
fine-pine <lsm3645@g.skku.edu>
2024-06-07 01:22:06 +0900

    fix: input id

--- FE/src/app/signup/page.tsx
+++ FE/src/app/signup/page.tsx
@@ -1,31 +1,31 @@
-"use client";
+'use client'
 
-import { FormEvent } from "react";
-import { signup } from "./_lib/api";
+import { FormEvent } from 'react'
+import { signup } from './_lib/api'
 
 export default function Page() {
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const username = e.currentTarget.username.value;
-    const studentId = Number(e.currentTarget.studentId.value);
-    const password = e.currentTarget.password.value;
-    const passwordConfirm = e.currentTarget.passwordConfirm.value;
+    e.preventDefault()
+    const username = e.currentTarget.username.value
+    const studentId = Number(e.currentTarget.studentId.value)
+    const password = e.currentTarget.password.value
+    const passwordConfirm = e.currentTarget['password-confirm'].value
 
     if (password !== passwordConfirm) {
-      alert("Password and Password Confirm are not same");
-      return;
+      alert('Password and Password Confirm are not same')
+      return
     }
 
-    const userInfo = signup(username, studentId, password);
-    alert(userInfo);
-  };
+    const userInfo = signup(username, studentId, password)
+    alert(userInfo)
+  }
 
   return (
     <main className="flex flex-col gap-8 text-center w-6c mx-auto">
       <h1 className="font-bold">Welcome to Eco Merge!</h1>
       <form action="#" className="flex flex-col gap-4" onSubmit={onSubmit}>
         <div className="flex items-end gap-4">
-          <label htmlFor="username">User Name</label>
+          <label htmlFor="username">Name</label>
           <input
             className="flex-1 border-solid border-b-2 border-black"
             type="text"
@@ -52,11 +52,11 @@ export default function Page() {
           />
         </div>
         <div className="flex items-end gap-4">
-          <label htmlFor="passwordConfirm">Password Confirm</label>
+          <label htmlFor="password-confirm">Password Confirm</label>
           <input
             className="flex-1 border-solid border-b-2 border-black"
             type="password"
-            id="passwordConfirm "
+            id="password-confirm"
             required
           />
         </div>
@@ -68,5 +68,5 @@ export default function Page() {
         </button>
       </form>
     </main>
-  );
+  )
 }
