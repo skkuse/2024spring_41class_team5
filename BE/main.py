0dfc20aa9d751e06529affe136e624292e48cf2f
yaongmeow <minnie_00@naver.com>
2024-06-11 13:19:40 +0900

    chore: delete footprint api files

--- BE/main.py
+++ BE/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
 from routes.auth import auth_router
-from routes.footprint import footprint_router
 from routes.green import green_router
 from routes.history import history_router
 from routes.statistics import statistics_router
@@ -30,7 +29,6 @@ app.add_middleware(
 )
 
 app.include_router(auth_router, prefix="/auth")
-app.include_router(footprint_router, prefix="/footprint")
 app.include_router(green_router, prefix="/green")
 app.include_router(history_router, prefix="/history")
 app.include_router(statistics_router, prefix="/statistics")
