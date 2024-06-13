4727e03320fc429de8664c9321d18bbc97ed7ee0
fine-pine <lsm3645@g.skku.edu>
2024-06-13 18:19:31 +0900

    style: add modal animation

--- FE/src/app/(dashboard)/editor/_components/EditorModal.tsx
+++ FE/src/app/(dashboard)/editor/_components/EditorModal.tsx
@@ -17,15 +17,28 @@ export default function EditorModal() {
   useEffect(() => {
     const accessToken = sessionStorage.getItem('accessToken')
     if (!accessToken) router.push('/login')
+
+    if (document.getElementById('overlay') && document.getElementById('modal')) {
+      document.getElementById('overlay').classList.remove('opacity-0')
+      document.getElementById('overlay').classList.add('opacity-50')
+      document.getElementById('modal').classList.remove('translate-y-96')
+      document.getElementById('modal').classList.add('-translate-y-1/2')
+      document.getElementById('modal').classList.remove('scale-75')
+      document.getElementById('modal').classList.add('scale-100')
+    }
   }, [])
 
   return (
     <>
       <div
-        className="fixed top-0 left-0 w-dvw h-dvh bg-black opacity-50 cursor-default z-10"
+        id="overlay"
+        className="fixed top-0 left-0 w-dvw h-dvh bg-black opacity-0 cursor-default z-10 transition-all duration-1000"
         onClick={onClose}
       />
-      <div className="fixed top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-10c p-4 bg-white rounded-xl z-20">
+      <div
+        id="modal"
+        className="fixed top-[50%] left-[50%] -translate-x-1/2 translate-y-96 scale-75 w-10c p-4 bg-white rounded-xl z-20 transition-all duration-1000"
+      >
         {originalCode == '' ? (
           <EditorWrapper setCode={setOriginalCode} setModifiedCode={setModifiedCode} />
         ) : (
